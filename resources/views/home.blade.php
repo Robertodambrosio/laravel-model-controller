40c47fa9048079eee72a01f9beffba99e33f4d94
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-02-04 16:36:39 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+
+<body>
+
+<div class="container">
+    @foreach ($movies as $item)
+    <div class="card">
+        <ul>
+            <h4>{{$item['title']}}</h4>
+            <li>
+                <p>{{$item['original_title']}}</p>
+            </li>
+            <li>
+                <p>{{$item['date']}}</p>
+            </li>
+            <li>
+                <h6>{{$item['vote']}}</h6>
+            </li>
+        </ul>
+    </div>
+    @endforeach
+</div>
+
+</body>
+
+</html>
